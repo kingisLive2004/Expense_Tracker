dc71326a43cd191e4003c12f5cdaf3950b946cf5
kingisLive2004 <bhattacharyasamik0206@gmail.com>
2025-08-18 03:29:24 +0530

    updated server.js

--- server.js
+++ server.js
@@ -2,38 +2,38 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const colors = require("colors");
 const path = require("path");
 const connectDb = require("./config/connectDb");
+
 // config dot env file
 dotenv.config();
 
-//databse call
+// database call
 connectDb();
 
-//rest object
+// rest object
 const app = express();
 
-//middlewares
+// middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-//routes
-//user routes
+// routes
 app.use("/api/v1/users", require("./routes/userRoute"));
-//transections routes
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
-//static files
+// static files (React frontend)
 app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (req, res) {
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-//port
-const PORT = 8080 || process.env.PORT;
 
-//listen server
+// port
+const PORT = process.env.PORT || 8080;
+
+// listen server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
